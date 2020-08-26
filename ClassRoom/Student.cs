18e4b3b29fb1120433ac20a1f99b7380fcf44bf0
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-08-26 12:44:26 +0200

    Task 1 - Student class added

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassRoom
+{
+    public class Student
+    {
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            BirthDay = birthDay;
+        }
+
+        public string Name { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+    }
+}
