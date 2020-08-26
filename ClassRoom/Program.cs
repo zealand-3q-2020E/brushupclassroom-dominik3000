5927637c3e8f7f63b974155e1ad04d6b8a70a06e
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-08-26 13:11:34 +0200

    Task 5 - Season method added

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -18,6 +18,50 @@ namespace ClassRoom
                 new Student("Andreas", 7, 2),
                 new Student("Justin", 12, 31)
             };
+
+
+
+            Console.WriteLine($"{OurClass.ClassName} started on {OurClass.SemesterStart.ToString("D")}\n");
+            Console.WriteLine("Students attending:");
+            foreach (var student in OurClass.ClassList)
+            {
+                Console.WriteLine("> "+student.Name);
+              
+            }
+
+            Console.WriteLine("\n\nPress any key to close the application...");
+            Console.ReadKey();
+
         }
+
+        public string Season(int monthOfBirth)
+        {
+
+            if (monthOfBirth == 12 || monthOfBirth ==1 || monthOfBirth ==2 )
+            {
+                return "Winter";
+            }
+
+
+            if (monthOfBirth == 3 || monthOfBirth == 4 || monthOfBirth == 5)
+            {
+                return "Spring";
+            }
+
+            if (monthOfBirth == 6 || monthOfBirth == 7 || monthOfBirth == 8)
+            {
+                return "Summer";
+            }
+
+            if (monthOfBirth == 9 || monthOfBirth == 10 || monthOfBirth == 11)
+            {
+                return "Autumn";
+            }
+
+            return "This dude was born in an impossible time.";
+        }
+
+
+
     }
 }
